e71b37d0bf6a5409f337957282030423f9868c13
Rajupatil9552 <rajupatil0199@gmail.com>
2025-11-18 14:44:52 +0530

    cors update

--- Server/middleware/cors.js
+++ Server/middleware/cors.js
@@ -3,7 +3,7 @@ const corsMiddleware = (req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
   
-  // Handle preflight requests
+  
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
